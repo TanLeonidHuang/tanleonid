14e53c80a0cc2332b578be7b8eeece737d77b265
Nathaniel Huang <nleonidhuang@gmail.com>
2023-06-09 03:10:00 -0400

    Day 1:
    Started off with a basic design which featured a phone like design.
    Used multiple pages to act as different "apps". Homepage was the home.
    projects.html would be the projects "app".
    
    Found inspo online for a smoother "app" transition that would fit the
    contents better. the "app" takes up the whole window now. Implemented
    the design into the existing homepage design.

---
+++ js/clock_script.js
@@ -0,0 +1,21 @@
+(function updateClock() {
+    var clockElement = document.getElementById('clock');
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var seconds = currentTime.getSeconds();
+
+    // Pad single-digit hours, minutes, and seconds with leading zeros
+    hours = (hours < 10 ? '0' + hours : hours);
+    hours = (hours>10 ? (hours - 12) : hours);
+    minutes = (minutes < 10 ? '0' + minutes : minutes);
+    seconds = (seconds < 10 ? '0' + seconds : seconds);
+
+    // Set the content of the clock element
+    clockElement.textContent = hours + ':' + minutes;
+
+    // Schedule the next update
+    setTimeout(updateClock, 1000);
+})();
+
+
